59a3d18dec733a44f9f7e29e3b621bc9072fb8b7
Anne <aheumann@students.uni-mainz.de>
2022-11-23 17:43:40 +0100

    Finishing main structure of elephant in streamlit

--- ELEphanT/EasyLanguageEvaluationTool/pages/01_ELEphanT_–_Analyze_Your_Text.py
+++ ELEphanT/EasyLanguageEvaluationTool/pages/01_ELEphanT_–_Analyze_Your_Text.py
@@ -27,6 +27,9 @@ with sidebar:
                 "which are commonly used in German literature, by German quotations („“).")
         st.markdown("- **Choseable evaluation modes**: \n 1. Easy Language rules only (default) \n 2. Easy "
                     "Language rules and additional text characteristics (i.e. number of tokens or words per sentence).")
+        st.markdown("- **Choseable Easy Language scores**: \n 1. Unweighted (default): Average number of fulfilled rules per text (in %)"
+                    "\n 2. Weighted: Unweighted score is multiplied with average rule compliance scores from an Easy Language reference corpus (in %)"
+                    "\n 3. Amount of perfect sentences: Percentage of sentences that fulfill at least 15 (of 16) rules (in %)")
         st.sidebar.image("./ELEphanT_logo.png", width=300)
     with exp2:
         st.markdown("- **Two kinds of results**: \n 1. Sentence level results, including the rule evaluations for every sentence."
@@ -48,47 +51,68 @@ with exp3:
     text_preprocessor = TextPreprocessor()
     preprocessed_text = text_preprocessor.preprocess_texts(text)
     st.markdown("##### Chose evaluation mode")
-    evaluation_mode = st.radio("Pick one", ("Easy Language rules only", "Easy Language rules and text characteristics"),
+    chosen_evaluation_mode = st.radio("Pick one", ("Easy Language rules only", "Easy Language rules and text characteristics"),
                                            label_visibility="collapsed")
-    #TODO: differentiate evaluation mode
-
-    #TODO: chose easy language score
+    st.markdown("##### Chose Easy Language score")
+    chosen_easy_language_score = st.radio("Pick one", ("Unweighted", "Weighted", "Amount of perfect sentences"),
+                               label_visibility="collapsed")
 if not text or not title:
     st.error("Insert your data!")
 elif text and title:
     #st.session_state["is_expanded"] = False
     #TODO: enable expander collapsing here?
-    st.markdown("##### Output – Results")
+    st.markdown("## Output – Results")
 
     easy_language_evaluator = EasyLanguageEvaluator()
     df_sentence_level_results = pd.DataFrame()
     df_text_level_results = pd.DataFrame()
+
     df_sentence_level_results, df_text_level_results = easy_language_evaluator.create_easy_language_results(
         preprocessed_text, title)
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.markdown("Unweighted Easy Language score")
-        st.markdown(f"### {df_text_level_results.iat[1, 7]}")
-    with col2:
-        st.markdown("Weighted Easy Language score")
-        st.markdown(f"### {df_text_level_results.iat[1, 8]}")
-    with col3:
-        st.markdown("Amount of perfect sentences")
-        st.markdown(f"### {df_text_level_results.iat[1, 10]}")
+    # text level dataframe modifications
+    df_text_level_results = df_text_level_results.dropna()
+    df_text_level_results = df_text_level_results.T
+    df_text_level_results = df_text_level_results.rename(index=str, columns={0: "Results"})
 
+    # sentence level dataframe modifications
+    df_sentence_level_results["Sentence"] = df_sentence_level_results["Sentence"].astype("str")
+    df_sentence_level_results["Number of Satisfied Rules abs."] = df_sentence_level_results[
+        "Number of Satisfied Rules abs."].astype(int)
+    df_sentence_level_results["Average Word Length (in Characters)"] = df_sentence_level_results[
+        "Average Word Length (in Characters)"].astype(float)
+    # Zum Rounding-problem: "round" scheint schon zu funktionieren, allerdings müssten die hintersten 0 abgeschnitten werden ...
+    df_sentence_level_results = df_sentence_level_results.sort_index()
+
+    # TODO: Insert evaluation Mode
+    if chosen_evaluation_mode == "Easy Language rules only":
+        pass
+    elif chosen_evaluation_mode == "Easy Language rules and text characteristics":
+        pass
+
+    # TODO: Grafik
+
+    if chosen_easy_language_score == "Unweighted":
+        unweighted_score = float(df_text_level_results.iat[7, 0])
+        st.markdown(f"**Unweighted Easy Language score: {unweighted_score}**")
+        st.markdown(f"### Your text complies with the rules of Easy Language to {'{:.1%}'.format(unweighted_score)}")
+    elif chosen_easy_language_score == "Weighted":
+        weighted_score = float(df_text_level_results.iat[8, 0])
+        st.markdown(f"**Weighted Easy Language score: {weighted_score}**")
+        st.markdown(
+                f"### Your text complies with the rules of Easy Language to {'{:.1%}'.format(weighted_score)}")
+    elif chosen_easy_language_score == "Amount of perfect sentences":
+        perfect_sentences_score = df_text_level_results.iat[10, 0]
+        st.markdown(f"**Unweighted Easy Language score: {perfect_sentences_score}**")
+        st.markdown(f"### {'{:.1%}'.format(perfect_sentences_score)} of the text's sentences comply at least to 15 rules")
     exp4 = st.expander("Preprocessed text")
     exp5 = st.expander("Sentence level results")
     exp6 = st.expander("Text level results", expanded=True)
     with exp4:
         st.markdown(preprocessed_text)
     with exp5:
-        df_sentence_level_results["Sentence"] = df_sentence_level_results["Sentence"].astype("str")
-        df_sentence_level_results["Number of Satisfied Rules abs."] = df_sentence_level_results[
-            "Number of Satisfied Rules abs."].astype(int)
-        df_sentence_level_results["Average Word Length (in Characters)"] = df_sentence_level_results[
-            "Average Word Length (in Characters)"].astype(float)
-        # Zum Rounding-problem: "round" scheint schon zu funktionieren, allerdings müssten die hintersten 0 abgeschnitten werden ...
-        st.dataframe(df_sentence_level_results)
+       st.dataframe(df_sentence_level_results)
     with exp6:
-        st.dataframe(df_text_level_results)
-        #TODO: delete row with only <NA>
+        st.table(df_text_level_results)
+
+    # Possible extensions
+    # Create graph with all sentences on y-axis and number of fulfilled rules on x-axis
