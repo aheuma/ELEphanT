6df6c88bfb81209b8746e7ff284fadae32e1b518
Anne <aheumann@students.uni-mainz.de>
2022-11-30 18:24:39 +0100

    Further progress on result presentation

--- ELEphanT/EasyLanguageEvaluationTool/pages/01_ELEphanT_–_Analyze_Your_Text.py
+++ ELEphanT/EasyLanguageEvaluationTool/pages/01_ELEphanT_–_Analyze_Your_Text.py
@@ -5,6 +5,7 @@ sys.path.append("./Model/")
 from TextPreprocessor import TextPreprocessor
 from EasyLanguageEvaluator import EasyLanguageEvaluator
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
@@ -65,7 +66,32 @@ elif text and title:
 
     # text level dataframe modifications
     df_text_level_results = df_text_level_results.dropna()
-    df_rel_text_level_results = df_text_level_results.iloc[0, [12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42]]
+    rules = ["High Numbers", "Percentage Numbers", "Written out Numbers", "Special Characters", "Repetitive Words",
+             "Long Words and Hyphens", "Abbreviations", "Nominalisations", "Passive Voice", "Genitive Case",
+             "Subjunctive Construct", "Negative Words", "Sentence Length", "Sentence Statements", "Complex Sentence Structure",
+             "Sentence Beginnings"]
+    scores = []
+    scores.append(df_text_level_results.iat[0, 12]*100)
+    scores.append(df_text_level_results.iat[0, 14]*100)
+    scores.append(df_text_level_results.iat[0, 16]*100)
+    scores.append(df_text_level_results.iat[0, 18]*100)
+    scores.append(df_text_level_results.iat[0, 20]*100)
+    scores.append(df_text_level_results.iat[0, 22]*100)
+    scores.append(df_text_level_results.iat[0, 24]*100)
+    scores.append(df_text_level_results.iat[0, 26]*100)
+    scores.append(df_text_level_results.iat[0, 28]*100)
+    scores.append(df_text_level_results.iat[0, 30]*100)
+    scores.append(df_text_level_results.iat[0, 32]*100)
+    scores.append(df_text_level_results.iat[0, 34]*100)
+    scores.append(df_text_level_results.iat[0, 36]*100)
+    scores.append(df_text_level_results.iat[0, 38]*100)
+    scores.append(df_text_level_results.iat[0, 40]*100)
+    scores.append(df_text_level_results.iat[0, 42]*100)
+    #scores = df_text_level_results.iloc[:, [12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42]]
+
+    df_rel_text_level_results = pd.DataFrame({"Rule": rules, "Compliance Score": scores})
+    df_rel_text_level_results["Compliance Score"] = df_rel_text_level_results["Compliance Score"].astype(int)
+
     df_text_level_results_transposed = df_text_level_results.T
     df_text_level_results_transposed = df_text_level_results_transposed.rename(index=str, columns={0: "Results"})
 
@@ -78,27 +104,42 @@ elif text and title:
     # Zum Rounding-problem: "round" scheint schon zu funktionieren, allerdings müssten die hintersten 0 abgeschnitten werden ...
     df_sentence_level_results = df_sentence_level_results.sort_index()
 
-    # TODO: hier weiter und index-Namen versuchen zu ändern!
-    #df_rel_text_level_results.columns = ["1: High Numbers", "2: Percentage Numbers", "3: Written out Numbers",
-    #"4: Special Characters", "5: Repetitive Words", "6: Long Words and Hyphens", "7: Abbreviations", "8: Nominalisations",
-    #"9: Passive Voice", "10: Genitive Case", "11: Subjunctive Construct", "12: Negative Words", "13: Sentence Length",
-    #"14: Sentence Statements", "15: Complex Sentence Structure", "16: Sentence Beginnings"]
-    st.dataframe(df_rel_text_level_results)
-    #st.bar_chart(df_rel_text_results)
+    col1, col2 = st. columns(2)
+    with col1:
+        st.dataframe(df_rel_text_level_results)
+        # Different Easy Language score outcome
+        if chosen_easy_language_score == "Unweighted":
+            unweighted_score = float(df_text_level_results.iat[0, 7])
+            st.markdown(f"**Unweighted Easy Language score: {unweighted_score}**")
+        elif chosen_easy_language_score == "Weighted":
+            weighted_score = float(df_text_level_results.iat[0, 8])
+            st.markdown(f"### Weighted Easy Language score: {weighted_score}")
+        elif chosen_easy_language_score == "Amount of perfect sentences":
+            perfect_sentences_score = df_text_level_results.iat[0, 10]
+            st.markdown(f"### Unweighted Easy Language score: {perfect_sentences_score}")
 
-    if chosen_easy_language_score == "Unweighted":
-        unweighted_score = float(df_text_level_results.iat[0, 7])
-        st.markdown(f"**Unweighted Easy Language score: {unweighted_score}**")
-        st.markdown(f"### Your text complies with the rules of Easy Language to {'{:.1%}'.format(unweighted_score)}")
-    elif chosen_easy_language_score == "Weighted":
-        weighted_score = float(df_text_level_results.iat[0, 8])
-        st.markdown(f"**Weighted Easy Language score: {weighted_score}**")
-        st.markdown(
+    with col2:
+        # Different Easy Language score outcome
+        if chosen_easy_language_score == "Unweighted":
+            st.markdown(
+                f"#### Your text complies with the rules of Easy Language to {'{:.1%}'.format(unweighted_score)}")
+        elif chosen_easy_language_score == "Weighted":
+            st.markdown(
                 f"### Your text complies with the rules of Easy Language to {'{:.1%}'.format(weighted_score)}")
-    elif chosen_easy_language_score == "Amount of perfect sentences":
-        perfect_sentences_score = df_text_level_results.iat[0, 10]
-        st.markdown(f"**Unweighted Easy Language score: {perfect_sentences_score}**")
-        st.markdown(f"### {'{:.1%}'.format(perfect_sentences_score)} of the text's sentences comply at least to 15 rules")
+        elif chosen_easy_language_score == "Amount of perfect sentences":
+            st.markdown(
+                f"### {'{:.1%}'.format(perfect_sentences_score)} of the text's sentences comply at least to 15 rules")
+        #TODO: Easy Language compliance score mehr hervorheben
+    # Create result plot
+    plt.style.use("ggplot")
+    x_pos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    plt.bar(x_pos, scores, color="steelblue")
+    plt.title("Rule Compliance in %")
+    plt.xticks(x_pos, rules)
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment="right")
+    st.pyplot(plt)
+
+
     exp4 = st.expander("Preprocessed text")
     exp5 = st.expander("Sentence level results")
     exp6 = st.expander("Text level results", expanded=True)
@@ -107,7 +148,4 @@ elif text and title:
     with exp5:
        st.dataframe(df_sentence_level_results)
     with exp6:
-        st.table(df_text_level_results)
-
-    # Possible extensions
-    # Create graph with all sentences on y-axis and number of fulfilled rules on x-axis
+        st.dataframe(df_text_level_results)
