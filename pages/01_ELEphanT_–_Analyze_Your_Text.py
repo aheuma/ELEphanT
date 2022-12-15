2a428544fb6890c15816f26c42f3eba605425167
Anne <aheumann@students.uni-mainz.de>
2022-12-15 11:21:13 +0100

    Update on ImageNotFoundError

--- pages/01_ELEphanT_–_Analyze_Your_Text.py
+++ pages/01_ELEphanT_–_Analyze_Your_Text.py
@@ -152,7 +152,7 @@ elif text and title:
             with expander_reference_scores:
                 tab_easy_language_reference_texts, tab_standard_german_reference_texts = st.tabs(["Easy Language Texts", "Standard German Texts"])
                 with tab_easy_language_reference_texts:
-                    dataframe_reference_easy_language = pd.read_excel("././Easy_Language_Reference_Texts.xlsx")
+                    dataframe_reference_easy_language = pd.read_excel("./xlsx-sheets/Easy_Language_Reference_Texts.xlsx")
                     if chosen_easy_language_score == "Unweighted":
                         dataframe_reference_easy_language = dataframe_reference_easy_language.drop(columns=["Weighted Score", "Amount of Perfect Sentences"])
                     elif chosen_easy_language_score == "Weighted":
@@ -161,7 +161,7 @@ elif text and title:
                         dataframe_reference_easy_language = dataframe_reference_easy_language.drop(columns=["Unweighted Score", "Weighted Score"])
                     st.dataframe(dataframe_reference_easy_language)
                 with tab_standard_german_reference_texts:
-                    dataframe_reference_standard_german = pd.read_excel("././Easy_Language_Reference_Texts.xlsx")
+                    dataframe_reference_standard_german = pd.read_excel("./xlsx-sheets/Standard_German_Reference_Texts.xlsx")
                     if chosen_easy_language_score == "Unweighted":
                         dataframe_reference_standard_german = dataframe_reference_standard_german.drop(columns=["Weighted Score", "Amount of Perfect Sentences"])
                     elif chosen_easy_language_score == "Weighted":
