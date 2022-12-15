e98807d8e08c477f669d2e83ae0e141cf371d8c0
Anne <aheumann@students.uni-mainz.de>
2022-12-15 12:05:19 +0100

    Update on ExcelFileNotFoundError

--- Start.py
+++ Start.py
@@ -12,6 +12,7 @@ with col2:
     st.markdown("# ELEphanT \n ### Easy Language Evaluation Tool")
     st.write("This page is a web presentation for the tool I developed during a university research project. "
                  "The tool offers the possibility to examine to what degree a given text complies to the rules of Easy Language. ")
+    #TODO: links updaten
     st.markdown("You will be able to: \n - Use the tool to [analyze](http://localhost:8501/ELEphanT_%E2%80%93_Analyze_Your_Text) "
                 "your own text \n - Learn about [Easy Language in Germany](http://localhost:8501/Information_on_Easy_Language) "
                 "\n - Read about the [details of my research project](http://localhost:8501/Information_on_Research_Project) on the basis of children's books")
