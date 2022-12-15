1c84f1b1aeb385cac88b0b204109d3c2102c4607
Anne <aheumann@students.uni-mainz.de>
2022-12-15 10:51:12 +0100

    Update on ImageNotFoundError

--- Start.py
+++ Start.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    elephant_logo = Image.open("../../ELEphanT_logo.png")
+    elephant_logo = Image.open("ELEphanT_logo.png")
     st.image(elephant_logo, width=440)
 with col2:
     st.markdown("# ELEphanT \n ### Easy Language Evaluation Tool")
